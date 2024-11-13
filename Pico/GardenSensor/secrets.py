4a78cb6207ffe7514c999586c5d1e8951393a5c4
pbussman <paul.bussman@hotmail.com>
2024-11-12 19:32:06 -0500

    Create secrets.py

---
+++ Pico/GardenSensor/secrets.py
@@ -0,0 +1,10 @@
+# secrets.py
+
+# WiFi credentials
+SSID = 'your_wifi_ssid'
+PASSWORD = 'your_wifi_password'
+
+# MQTT broker credentials
+MQTT_SERVER = 'your_mqtt_server_address'
+MQTT_USERNAME = 'your_mqtt_username'
+MQTT_PASSWORD = 'your_mqtt_password'
