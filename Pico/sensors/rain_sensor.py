581875372d1f750fedfebf389f20b24c649009b5
pbussman <paul.bussman@hotmail.com>
2025-07-01 20:30:41 -0400

    Update rain_sensor.py

--- Pico/sensors/rain_sensor.py
+++ Pico/sensors/rain_sensor.py
@@ -1,14 +1,25 @@
-from machine import Pin
-import utime
+import machine, time, json
+from umqtt.simple import MQTTClient
 
-class RainSensor:
-    def __init__(self, power_pin, data_pin):
-        self.power_pin = Pin(power_pin, Pin.OUT)
-        self.data_pin = Pin(data_pin, Pin.IN)
+MQTT_BROKER = "your.mqtt.broker"
+SENSOR_NAME = "roof1"
+CLIENT_ID = "rain_" + SENSOR_NAME
 
-    def read(self):
-        self.power_pin.value(1)  # Turn the rain sensor's power ON
-        utime.sleep_ms(10)  # Wait 10 milliseconds
-        rain_state = self.data_pin.value()
-        self.power_pin.value(0)  # Turn the rain sensor's power OFF
-        return "It's raining!" if rain_state == 0 else "No rain."
+pin = machine.ADC(28)
+
+def read_and_publish():
+    value = pin.read_u16()  # Range varies by sensor
+    rain_detected = value < 30000
+
+    client = MQTTClient(CLIENT_ID, MQTT_BROKER)
+    client.connect()
+
+    client.publish(f"garden/sensor/{SENSOR_NAME}/rain_detected", json.dumps({
+        "value": rain_detected
+    }))
+
+    client.disconnect()
+
+while True:
+    read_and_publish()
+    time.sleep(300)
