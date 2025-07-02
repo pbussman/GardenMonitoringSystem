b4efd7c6bdadd5ae73aecc0d823e59bae99e8e96
pbussman <paul.bussman@hotmail.com>
2025-07-01 22:21:00 -0400

    Update decision_engine.py

--- SBC/Aggragator/Main/ml/decision_engine.py
+++ SBC/Aggragator/Main/ml/decision_engine.py
@@ -1,1 +1,7 @@
+def get_irrigation_advice(zone, recent_readings):
+    moisture = recent_readings.get("soil_moisture", 100)
+    if moisture < 30:
+        return {"action": "water", "duration_min": 12}
+    else:
+        return {"action": "skip"}
 
