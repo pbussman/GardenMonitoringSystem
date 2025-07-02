8b815b5da621f693825bc49fe7a160e87087cd32
pbussman <paul.bussman@hotmail.com>
2025-07-01 21:03:24 -0400

    Update dht22_sensor.py

--- Pico/sensors/dht22_sensor.py
+++ Pico/sensors/dht22_sensor.py
@@ -1,9 +1,13 @@
 from config import load_config
+from mqtt_helper import connect_mqtt
 from umqtt.simple import MQTTClient
 import machine, time, json
 import dht
 
+# Load settings from config.json
 cfg = load_config()
+
+# Set up sensor
 sensor = dht.DHT22(machine.Pin(15))
 
 def read_and_publish():
@@ -12,23 +16,19 @@ def read_and_publish():
         temp = sensor.temperature()
         humidity = sensor.humidity()
 
-        client = MQTTClient(
-            client_id=cfg["client_id"],
-            server=cfg["mqtt_broker"],
-            port=cfg["mqtt_port"],
-            user=cfg["username"],
-            password=cfg["password"]
-        )
-
+        client = connect_mqtt(cfg)
         client.connect()
+
         client.publish(f"garden/sensor/{cfg['sensor_name']}/air_temperature", json.dumps({
             "value": temp,
             "unit": "°C"
         }))
+
         client.publish(f"garden/sensor/{cfg['sensor_name']}/humidity", json.dumps({
             "value": humidity,
             "unit": "%"
         }))
+
         client.disconnect()
 
     except Exception as e:
