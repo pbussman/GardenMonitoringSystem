0164faf140876820c3af3288d7825926231a44a6
pbussman <paul.bussman@hotmail.com>
2025-07-02 21:01:19 -0400

    Create beds_config.py

---
+++ nodes/beds_config.py
@@ -0,0 +1,24 @@
+{
+  "sensor_name": "bed_valves",
+  "mqtt": {
+    "broker": "192.168.1.100",
+    "port": 1883,
+    "client_id": "node_bed_valves",
+    "username": "garden",
+    "password": "growgreen"
+  },
+  "relays": {
+    "bed1": 0,
+    "bed2": 1,
+    "bed3": 2,
+    "bed4": 3,
+    "bed5": 4
+  },
+  "flow_sensors": {
+    "bed1": 5,
+    "bed2": 6,
+    "bed3": 7,
+    "bed4": 8,
+    "bed5": 9
+  }
+}
