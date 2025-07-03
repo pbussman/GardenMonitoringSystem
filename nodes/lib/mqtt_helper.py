5c8ee1df6e38e9970a70f1cadfd92c62926557e0
pbussman <paul.bussman@hotmail.com>
2025-07-02 21:09:44 -0400

    Update mqtt_helper.py

--- nodes/lib/mqtt_helper.py
+++ nodes/lib/mqtt_helper.py
@@ -1,12 +1,21 @@
-# mqtt_helper.py
+import json
 
-from umqtt.simple import MQTTClient
+def load_config(filename="config.json"):
+    with open(filename, "r") as f:
+        config = json.load(f)
 
-def connect_mqtt(cfg):
-    return MQTTClient(
-        client_id=cfg["client_id"],
-        server=cfg["mqtt_broker"],
-        port=cfg["mqtt_port"],
-        user=cfg["username"],
-        password=cfg["password"]
-    )
+    sensor_name = config.get("sensor_name", "default_sensor")
+    mqtt_cfg = config.get("mqtt", {})
+    relays = config.get("relays", {})
+    flow_sensors = config.get("flow_sensors", {})
+
+    return {
+        "sensor_name": sensor_name,
+        "mqtt_broker": mqtt_cfg.get("broker", "localhost"),
+        "mqtt_port": mqtt_cfg.get("port", 1883),
+        "client_id": mqtt_cfg.get("client_id", f"{sensor_name}_client"),
+        "username": mqtt_cfg.get("username", ""),
+        "password": mqtt_cfg.get("password", ""),
+        "relays": relays,
+        "flow_sensors": flow_sensors
+    }
